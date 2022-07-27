f3046b5d1581ec4d168be7c3f81b222594259372
JanisPlayer <54918417+JanisPlayer@users.noreply.github.com>
2022-07-27 04:38:56 +0200

    cookie bug fix, add MD5 Support for GET

--- Client/web_site_socket.js
+++ Client/web_site_socket.js
@@ -9,6 +9,10 @@ var last_username;
 
 function sendMessage(currentTime, play, channel, password, username, chat, videoid) {
 
+if (password_is_MD5 != true) {
+    password = MD5(password);
+}
+
   // Create WebSocket connection.
   // Connection opened
 
